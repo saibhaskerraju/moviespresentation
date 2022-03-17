8e5b80b0f4a5d5b944a61252d75d040343e6538a
Sai Bhasker Raju <saibhaskerraju@outlook.com>
2022-03-16 22:36:31 -0400

    adding video

---
+++ src/app/landings/landings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landings',
+  templateUrl: './landings.component.html',
+  styleUrls: ['./landings.component.css']
+})
+export class LandingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
